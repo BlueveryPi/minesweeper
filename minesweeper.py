b91c60e96beb81ec883043c615e33f751b59eca3
BlueveryPi <78254389+BlueveryPi@users.noreply.github.com>
2021-07-03 22:48:58 +0900

    added flagging

--- minesweeper.py
+++ minesweeper.py
@@ -3,21 +3,24 @@ pygame.init()
 
 size=6
 screen = pygame.display.set_mode([size*50+30+(size-1)*5, size*50+30+(size-1)*5])
-minelist=[]
+gridlist=[]
 
 class mine():
     def __init__(self):
         self.x=0
         self.y=0
+        self.flagged=False
+        self.mine=False
+        self.triggered=False
 
 for p in range(size):
-    minelist.append([])
+    gridlist.append([])
 
 for p in range(size):
     for q in range(size):
-        minelist[p].append(mine())
-        minelist[p][q].x=p
-        minelist[p][q].x=q
+        gridlist[p].append(mine())
+        gridlist[p][q].x=p
+        gridlist[p][q].x=q
 
 running = True
 while running:
@@ -25,13 +28,25 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONUP: 
+            if event.button==3: #if left click
+                if event.pos[0]>15 and event.pos[0]<size*50+30+(size-1)*5-15 and event.pos[1]>15 and event.pos[1]<size*50+30+(size-1)*5-15: #some nice over-safety
+                    gridlist[int((event.pos[0]-2.5)//56)][int((event.pos[1]-2.5)//56)].flagged ^= True #toggle the thingy
 
     screen.fill((255, 255, 255))
 
     for x in range(size):
         for y in range(size):
-            pygame.draw.rect(screen, (33, 118, 255), pygame.Rect(15+x*50+5*x, 15+y*50+5*y, 50, 50))
-            pygame.draw.rect(screen, (18, 109, 255), pygame.Rect(18+x*50+5*x, 18+y*50+5*y, 44, 44))
+            if gridlist[x][y].flagged==False:
+                color=(33, 118, 255)
+            else:
+                color=(255, 26, 10)
+
+            pygame.draw.rect(screen, color, pygame.Rect(15+x*50+5*x, 15+y*50+5*y, 50, 50)) #draw the thingy
+            pygame.draw.rect(screen, (240, 240, 240), pygame.Rect(18+x*50+5*x, 18+y*50+5*y, 44, 44))
+
+            if gridlist[x][y].mine==True and gridlist[x][y].triggered==True: #if boom
+                pygame.draw.rect(screen, (255, 26, 10), pygame.Rect(18+x*50+5*x, 18+y*50+5*y, 44, 44)) #color red
 
     pygame.display.flip()
 
