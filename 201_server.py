38097d27762023ed3e8a6c45499cbef446a99234
BlueveryPi <78254389+BlueveryPi@users.noreply.github.com>
2021-08-14 17:53:04 +0900

    Update 201_server.py

--- 201_server.py
+++ 201_server.py
@@ -2,21 +2,39 @@
 
 from socket import *
 import threading
-import time 
+import time
 
-def send(sock):
+connections=[]
+def acceptall():
     while True:
-        sendData = input(">>> ")
-        if sendData != "close":
-            sock.send(sendData.encode("utf-8"))
+        connectionSocket, addr = serverSocket.accept()
+        thisguysnick=connectionSocket.recv(1024)
+        print(f"{thisguysnick.decode('utf-8')}님이 접속했습니다.")
+        for sock in connections:
+            if sock != connectionSocket:
+                sock.send((f"{thisguysnick.decode('utf-8')}님이 접속했습니다.").encode('utf-8'))
+        connections.append(connectionSocket)
+        sender = threading.Thread(target=send, args=(connections, ))
+        receiver = threading.Thread(target=recv, args=(connectionSocket, connections, ))
+        sender.start()
+        receiver.start()
+
+def send(socks):
+    while True:
+        sendData = input("")
+        if sendData != "serveroff":
+            for sock in socks:
+                sock.send(("<System>: "+sendData).encode('utf-8'))
         else:
             serverSocket.close() 
 
-def recv(sock):
+def recv(sock, socks):
     while True:
-        data = sock.recv(1024)
-        print("\n상대방1:", data.decode("utf-8"))
-        print(">>> ")
+        data=sock.recv(1024)
+        print(data.decode('utf-8'))
+        for sock1 in socks:
+            if sock1 != sock:
+                sock1.send(data)
 
 host = '127.0.0.1'
 port = 65432
@@ -25,27 +43,5 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind( (host, port))
 serverSocket.listen(1)
 print("=== 대기중입니다.")
-serverSocket1 = socket(AF_INET, SOCK_STREAM)
-serverSocket1.bind( (host, port+1))
-serverSocket1.listen(1)
-print("=== 1대기중입니다.")
 
-connectionSocket, addr = serverSocket.accept()
-print("{}에서 접속했습니다.".format(str(addr)))
-connectionSocket1, addr1 = serverSocket1.accept()
-print("{}에서 접속했습니다1.".format(str(addr1)))
-
-sender = threading.Thread(target=send, args=(connectionSocket,))
-receiver = threading.Thread(target=recv, args=(connectionSocket,))
-sender1 = threading.Thread(target=send, args=(connectionSocket1,))
-receiver1 = threading.Thread(target=recv, args=(connectionSocket1,))
-
-sender.start()
-receiver.start()
-sender1.start()
-receiver1.start()
-
-'''
-한 개의 접속만 유지하는 채팅 서버 프로그램
-    - 간단, 
-'''
+acceptall()
